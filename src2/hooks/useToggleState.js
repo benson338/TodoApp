8fe47ba082550f79ba927cca1d29e2a847f97fdf
BMK <bensonmkurian@gmail.com>
2022-01-27 19:16:31 +0530

    2-with-context

--- src2/hooks/useToggleState.js
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-
-export default function useToggleState(intialVal = false) {
-  // call useState, "reserve piece of state"
-  const [state, setState] = useState(intialVal);
-  const toggle = () => setState(!state);
-  // return piece of state & () to toggle it
-  return [state, toggle];
-}
